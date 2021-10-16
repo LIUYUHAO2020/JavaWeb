b357ad623aaf9f39624b04a4f5615a51c8a16518
liuyuhao <2103674853@qq.com>
2021-10-16 16:27:31 +0800

    Add JavaWebTest

---
+++ src/main/java/com/neu/edu/Hello.java
@@ -0,0 +1,10 @@
+package com.neu.edu;
+
+public class Hello {
+    public Hello() { }
+
+    @Override
+    public String toString(){
+        return "Hello world!!!!!!!+12";
+    }
+}
---
+++ src/test/java/com/neu/edu/HelloTest.java
@@ -0,0 +1,11 @@
+package com.neu.edu;
+
+import org.junit.Test;
+
+public class HelloTest {
+    @Test
+    public void HelloTest(){
+        Hello hello = new Hello();
+        System.out.println(hello.toString()+"Hello Git");
+    }
+}
